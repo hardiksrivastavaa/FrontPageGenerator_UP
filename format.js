ed4cc031f7f7c8fa0d8c929a3f0faacd689a965a
Hardik Srivastava <144905418+Hardik-Techie@users.noreply.github.com>
2024-06-08 15:01:29 +0530

    Add files via upload

---
+++ format.js
@@ -0,0 +1,24 @@
+var college = sessionStorage.getItem("college");
+document.getElementById("college").textContent = college;
+
+var session = sessionStorage.getItem("session");
+document.getElementById("session").textContent = `Session : ${session}`;
+
+var subject = sessionStorage.getItem("subject");
+document.getElementById("subject").textContent = subject;
+
+var branch = sessionStorage.getItem("branch");
+document.getElementById("branch").textContent = branch;
+
+var yearSem = sessionStorage.getItem("yearSem");
+document.getElementById("yearSem").textContent = yearSem;
+
+var teacher = sessionStorage.getItem("teacher");
+document.getElementById("teacher").textContent = teacher;
+
+var stdname = sessionStorage.getItem("name");
+document.getElementById("stdname").textContent = stdname;
+
+var enrollment = sessionStorage.getItem("enrollment");
+document.getElementById("enrollment").textContent = enrollment;
+
